d0ca715e60a4a41311bfadc3635bd9dda7a50b82
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-18 17:47:51 +0200

    Edit ex00: match subject

--- ex00/libft_creator.sh
+++ ex00/libft_creator.sh
@@ -1,2 +1,2 @@
-gcc -Wall -c *.c
-ar -cvq libft.a *.o
+gcc -Wall -c ft_putchar.c ft_putstr.c ft_strcmp.c ft_strlen.c ft_swap.c
+ar -cvq libft.a ft_putchar.o ft_putstr.o ft_strcmp.o ft_strlen.o ft_swap.o
